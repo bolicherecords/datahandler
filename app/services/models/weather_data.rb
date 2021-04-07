85865f9976befb4e8b5476f3df92f4097f645bc2
BolicheRecords <bolicherecords@gmail.com>
2021-04-07 11:08:21 -0400

    dev fix sunset and sunrise

--- app/services/models/weather_data.rb
+++ app/services/models/weather_data.rb
@@ -9,12 +9,13 @@ module Models
     def create_weather
       main = @weather.dig('main')
       wind = @weather.dig('wind')
+      sys = @weather.dig('sys')
       Weather.create(
         pressure: main.dig('pressure'),
         temperature: main.dig('temp'),
         humidity: main.dig('humidity'),
-        sunset: main.dig('pressure'),
-        sunrise: main.dig('pressure'),
+        sunset: sys.dig('sunset'),
+        sunrise: sys.dig('sunrise'),
         wind_speed: wind.dig('speed'),
         wind_deg: wind.dig('deg'),
         event_id: @historical.id,
